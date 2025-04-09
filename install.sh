39724748a97390a4565268dd06e6aaa01e58adf4
Sahil Bajaj <bajaj@Sahils-MacBook-Pro-2.local>
2025-04-09 14:39:29 +0530

    Add custom setup and pytorch installation

---
+++ install.sh
@@ -0,0 +1,7 @@
+apt-get update && \
+    apt-get install -y --no-install-recommends git wget build-essential libgl1 libglib2.0-0 libsm6 libxext6 libxrender-dev -y && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+pip install -r requirements.txt
+
